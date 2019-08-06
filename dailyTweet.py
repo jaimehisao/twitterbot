5a4021258faccd5ec90b8a08a0a1da5fa5df85a7
Hisao <jaimhisao2011@gmail.com>
2019-08-06 01:09:41 -0500

    Added generation functionality

--- dailyTweet.py
+++ dailyTweet.py
@@ -6,8 +6,17 @@ Version 0.1
 '''
 import tweepy
 import logging
+import re #Regex
 from config import create_api
-import textgenrnn
+from textgenrnn import textgenrnn
+from config import create_api
+
+'''
+
+We could also add a functionality that can, based on a Tweet add more users to base our tweets upon
+or have an option to add in a "per tweet" basis, to the training model.
+
+
 
 #Program reads file-stored tweets, 
 def readFromFile():
@@ -16,9 +25,46 @@ def readFromFile():
 def writeToFile():
     
     print()
+'''
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+
+api = create_api()
+
+texts = []
+contextLabels = []
+
+def processText(text):
+    text = re.sub(r'http\S+', '', text)   #Removes URLs included in the Tweet
+    text = re.sub(r'@[a-zA-Z0-9_]+', '', text)  #Remove @ mentions in Tweets
+    text = text.strip(" ")   #Remove whitespace characters resulting from previus operations
+    text = re.sub(r' +', ' ', text)   #Remove redundant spaces (extra)
+
+    #Handle common HTML entities
+    text = re.sub(r'&lt;', '<', text)
+    text = re.sub(r'&gt;', '>', text)
+    text = re.sub(r'&amp;', '&', text)
+    return text
 
 def generateTweet():
-    print()
+    print("Downloading user Tweets...")
+    all_tweets = tweepy.Cursor(api.user_timeline, screen_name = 'JHisao', count = 200, tweet_mode = 'extended', include_rts = False).pages(16)
+
+    for page in all_tweets:
+        for tweet in page:
+            tweetText = processText(tweet.full_text)
+            if tweetText is not '':
+                texts.append(tweetText)
+                contextLabels.append('JHisao')
+    
+
+    #textgen = textgenrnn(name='{}_twitter'.format("_".join(cfg['twitter_users'])))
+    textgen = textgenrnn(name= "JHisao_twitter")
+
+    textgen.train_new_model(texts, context_labels = contextLabels, gen_epochs = 200, batch_size = 128, train_size= 1.28, rnn_layers = 2, rnn_size = 128, rnn_bidirectional = False, max_length = 40, dim_embeddings = 100, word_level = False)
+    
+
 
 #Use file with stored Tweets to reduce API strain?
 def dailyBasedTweet():
--- main.py
+++ main.py
@@ -11,12 +11,14 @@ from followBack import follow
 from followBack import unfollow
 from likeMyTweets import likeMTweets
 from mentions import handleMentions
+from dailyTweet import dailyBasedTweet
 
 #Scheduling for tasks that the bot has tu run
 schedule.every().day.at("12:00").do(follow) #Follows Back users that follow the bot
 schedule.every().day.at("1:00").do(unfollow) #Unfollows users that follow the bot
 schedule.every().day.at("18:00").do(likeMTweets) #Likes my other account's tweets
 schedule.every().hour.at(":40").do(handleMentions) #Handles Tweet Mentions
+schedule.every().day.at("9:00").do(dailyBasedTweet) #Handles Every Day Tweet
 
 while True:
     schedule.run_pending()
