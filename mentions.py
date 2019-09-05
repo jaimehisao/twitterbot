7a364316bc45e16d2cbfc618270afa2a033300bb
Jaime Hisao <jaimhisao2011@gmail.com>
2019-09-04 23:54:14 -0500

    Reduced to 10 the number of tweets queried for mentions

--- mentions.py
+++ mentions.py
@@ -16,16 +16,14 @@ def check_mentions(api, keywords, since_id):
 
     logger.info("Retrieving Tweets and Checking for Mentions...")
     new_since_id = since_id
-    for tweet in tweepy.Cursor(api.mentions_timeline, since_id=since_id, count = 20).items():
+    for tweet in tweepy.Cursor(api.mentions_timeline, since_id=since_id, count = 10).items():
         new_since_id = max(tweet.id, new_since_id)
         if tweet.in_reply_to_status_id is not None:
             continue
         if any(keyword in tweet.text.lower() for keyword in keywords):
             logger.info(f"Answering to", tweet.user.name)
             api.update_status(
-                status = "Hola!",
-                in_reply_to_status_id = tweet.id,
-            )
+                status = "Hola!", in_reply_to_status_id = tweet.id)
     return new_since_id
 
 
