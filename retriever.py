92608409d7df919edb764df0a4a95610b13d2f46
Jaime Hisao <jaimhisao2011@gmail.com>
2019-11-17 17:08:27 -0600

    fix: retriever funtionality

--- retriever.py
+++ retriever.py
@@ -4,6 +4,7 @@ from tweepy import Cursor
 from datetime import datetime, date, time, timedelta
 from collections import Counter
 import sys
+import uuid
 
 #Homemade Classes import
 from config import create_api
@@ -12,33 +13,35 @@ import mongoer
 #API Object from our keys.
 api = create_api()
 
-#Account lists to query from the Twitter API
-accountsToQuery = ['anaortegon3']
+#Retrieve User's information to add to the database
+def retrieveBasicInfo() -> None:
+    pass
 
-#Queries the accounts of every screen name in the array above
-#Returns basic information about the user's account
-if len(accountsToQuery) > 0:
-    for target in accountsToQuery:
-        print("Getting data for " + target)
-        item = api.get_user(target)
-        print("name: " + item.name)
-        print("screen_name: " + item.screen_name)
-        print("description: " + item.description)
-        print("statuses_count: " + str(item.statuses_count))
-        print("friends_count: " + str(item.friends_count))
-        print("followers_count: " + str(item.followers_count))
+def retrieveUsersTweets() -> None:
+    #Open Connection to MongoDB and sets the collection to be used
+    mongo = mongoer.Mongo()
+    userTweets = mongo.returnTwitterUserTweetsCollection()
+    optedInDatabase = mongo.returnOptedInUsersCollection()
+    #tweetNumber = userTweets.find({}).sort({"tweetNumber":-1}).limit(1)
+    #latestTweet = userTweets.find({}).sort({"timestamp":-1}).limit(1)
 
-        #Tweet Query in a per user basis
-        hashtags = []
-        mentions = []
-        tweet_count = 0
-        #Gets the current time and sets a 30 day back timer.
-        end_date = datetime.utcnow() - timedelta(days=30)
-        #Returns up to 3200 tweets from the queried user.
-        for status in Cursor(api.user_timeline, id=target).items():
-            tweet_count += 1
+    #optedInDatabase.insert_one({'_id' : str(uuid.uuid3(name = 'anaortegon3', namespace = uuid.NAMESPACE_DNS)), 'screenName' : 'anaortegon3', 'addedAt' : datetime.utcnow()})
+    #Retrieves all opted in users in the MongoDB Database and iterates over every single one.
+    for user in optedInDatabase.find({}):
+        item = api.get_user(user['screenName'])
+        print('Inserting Tweets from...'+ user['screenName'])
+        #For every Tweet from the user in the OptedIn database
+        for status in Cursor(api.user_timeline, id=user['screenName']).items():
+            uuidEx = str(uuid.uuid3(name = str(status.text.lower()), namespace = uuid.NAMESPACE_DNS))
+            nameOp = item.name
+            screen = item.screen_name
+            timestamp = status.created_at
+            userMentions = []
+            hashtags = []
+            statuses = item.statuses_count
+            statusText = status.text
+            #Checks if there are users mentioned or hashtags used and adds them to array
             if hasattr(status, "entities"):
-                print(status.text)
                 entities = status.entities
                 if "hashtags" in entities:
                     for ent in entities["hashtags"]:
@@ -53,20 +56,29 @@ if len(accountsToQuery) > 0:
                             if "screen_name" in ent:
                                 name = ent["screen_name"]
                                 if name is not None:
-                                    mentions.append(name)
-            if status.created_at < end_date:
+                                    userMentions.append(name)
+
+            #Checks if the Tweet is a Retweet based in the first two char of the string.
+            if(statusText[0] == 'R' and statusText[1] == 'T'):
+                isRetweet = True
+                print("Is RT")
+            else:
+                isRetweet = False
+                print("Isnt RT")
+
+            #Checks if the Tweet isn't already inserted in the DB using the UUID
+            if not userTweets.find_one({"_id" : uuidEx}):
+                tmpTweet = {'_id' : uuidEx, 'name' : nameOp, 'screenName' : screen, 'text' : statusText,
+                        'timestamp' : timestamp, 'userMentions' : userMentions, 'hashtags' : hashtags,
+                        'isRetweet' : isRetweet, 'statusesCount' : statuses}
+                userTweets.insert_one(tmpTweet)
+                print("Inserting Tweet from " + item.name)
+            else:
+                print("Not Inserting Tweet from " + item.name)
+                print("Moving On To Next User... ")
                 break
-        
-        print
-        print("Most mentioned Twitter users:")
-        for item, count in Counter(mentions).most_common(10):
-            print(item + "\t" + str(count))
 
-        print
-        print("Most used hashtags:")
-        for item, count in Counter(hashtags).most_common(10):
-            print(item + "\t" + str(count))
+    #Deletes the Mongo object, which closes the connection to the database.
+    del mongo
 
-        print
-        print ("All done. Processed " + str(tweet_count) + " tweets.")
-        print
+retrieveUsersTweets()
