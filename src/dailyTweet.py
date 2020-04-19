3060db9b51fa3d75f8c54f659bbd4763a7232dde
Hisao <jaimehisao2011@gmail.com>
2020-04-18 23:42:44 -0500

    Added Travis File

--- src/dailyTweet.py
+++ src/dailyTweet.py
@@ -1,9 +1,9 @@
-'''
-dailyTweet.py by Jaime Hisao Yesaki 
+"""
+dailyTweet.py by Jaime Hisao Yesaki
 This program uses my main Twitter account as well as other accounta as templates for new auto-generated tweets.
 Created August 5, 2019
 Version 0.1
-'''
+"""
 import tweepy
 import logging
 import re  # Regex
