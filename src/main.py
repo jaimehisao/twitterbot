04e48fe54ac2b2aa4bb8b92be11d306c8e9f59f1
Hisao <jaimehisao2011@gmail.com>
2020-07-03 02:09:03 -0500

    Changed time when programs execute. Likes are done every 47 minutes and tweets are queried every hour at minute 01

--- src/main.py
+++ src/main.py
@@ -21,12 +21,12 @@ from tweets.retrieverPostgres import retrieve_user_tweets
 # Scheduling for tasks that the bot has tu run
 # schedule.every().day.at("22:30").do(follow) #Follows Back users that follow the bot
 # schedule.every().day.at("01:35").do(un_follow) #Unfollows users that follow the bot
-# schedule.every().hour.at(":25").do(like_my_tweets)  # Likes my other (main) account's tweets
+schedule.every().hour.at(":47").do(like_my_tweets)  # Likes my other (main) account's tweets
 # schedule.every().hour.at(":00").do(handle_mentions) #Handles Tweet Mentions at each hour
 # schedule.every().hour.at(":45").do(handle_mentions) #Handles Tweet Mentions at each 45 minute of hour
 # schedule.every().day.at("09:00").do(daily_based_tweets) #Handles Every Day Tweet
 
-schedule.every().hour.at(":32").do(retrieve_user_tweets)  # Download new tweets
+schedule.every().hour.at(":01").do(retrieve_user_tweets)  # Download new tweets
 
 # This will run always, checking the scheduler to see if a piece of code has to run at the specified hour and minute,
 # otherwise, go to sleep
